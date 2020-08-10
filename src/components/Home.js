cc0c4fdad8082eef6117bcaa9fc19b8b7b1a49b1
Manu Manoj <manumanoj4540@gmail.com>
2020-08-10 11:22:20 -0400

    Updated Contents

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,37 +14,37 @@ function Home() {
                 rating = {4}
                 />
                 <Product 
-                title= "The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                image = "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-                price = {11.96}
+                title= "Kenwood Mix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+                image = "https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+                price = {130.99}
+                rating = {3}
+                />
+            </div>
+            <div className="home__row">
+                <Product 
+                title= "Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+                image = "https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+                price = {120.99}
+                rating = {4}
+                />
+                <Product 
+                title= "Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+                image = "https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+                price = {60.96}
+                rating = {4}
+                />
+                <Product 
+                title= "New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+                image = "https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+                price = {999.96}
                 rating = {4}
                 />
             </div>
             <div className="home__row">
                 <Product 
-                title= "The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                image = "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-                price = {11.96}
-                rating = {4}
-                />
-                <Product 
-                title= "The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                image = "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-                price = {11.96}
-                rating = {4}
-                />
-                <Product 
-                title= "The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                image = "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-                price = {11.96}
-                rating = {4}
-                />
-            </div>
-            <div className="home__row">
-                <Product 
-                title= "The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-                image = "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-                price = {11.96}
+                title= "Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+                image = "https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+                price = {399.96}
                 rating = {4}
                 />
             </div>
