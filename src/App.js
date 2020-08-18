217fe1a8709216d35f103124bee3c645eac7e207
Manu Manoj <manumanoj4540@gmail.com>
2020-08-18 16:31:35 -0400

    Updated Login Comp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import Checkout from './components/Checkout';
-import Login  from './components/Login'
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useStateValue } from "./components/StateProvider";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Checkout from "./components/Checkout";
+import Login from "./components/Login";
+import { auth } from "./firebase";
+import "./App.css";
 
 function App() {
+  const [user, dispatch] = useStateValue();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        dispatch({
+          type: "SET__USER",
+          user: authUser
+        });
+      } else {
+        dispatch({
+          type: "SET__USER",
+          user: null
+        });
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/checkout"><Header /><Checkout /></Route>
-          <Route path="/login"><Login /></Route>
-          <Route path="/"><Header /><Home /></Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
         </Switch>
       </div>
     </Router>
--- src/components/reducer.js
+++ src/components/reducer.js
@@ -1,29 +1,37 @@
+import { compose } from "@material-ui/system";
+
 export const initialState = {
-    basket : [],
-    user : null
-}
+    basket: [],
+    user: {}
+};
 
-export const getBasketTotal = (basket) => basket?.reduce((amount,item) => item.price+amount, 0);
+export const getBasketTotal = basket =>
+    basket?.reduce((amount, item) => item.price + amount, 0);
 
-const reducer = (state,action) => {
-    switch(action.type){
-        case 'ADD_TO_BASKET' :
-            return {
-                ...state,
-                basket: [...state.basket, action.item]
-                };
-            break;
-        case 'REMOVE_FROM_BASKET':
-            let newBasket = [...state.basket];
-            const index = state.basket.findIndex((item) => item.id === action.id);
-            if(index >= 0){
-                newBasket.splice(index,1);
-            }
-            return {...state, basket: newBasket };
-            break;
-        default :
-            return state;
+const reducer = (state, action) => {
+    switch (action.type) {
+    case "SET__USER":
+        return {
+        ...state,
+        user: action.user
+        };
+    case "ADD_TO_BASKET":
+        return {
+        ...state,
+        basket: [...state.basket, action.item]
+        };
+        break;
+    case "REMOVE_FROM_BASKET":
+        let newBasket = [...state.basket];
+        const index = state.basket.findIndex(item => item.id === action.id);
+        if (index >= 0) {
+            newBasket.splice(index, 1);
+        }
+        return { ...state, basket: newBasket };
+        break;
+    default:
+        return state;
     }
-}
+};
 
 export default reducer;
